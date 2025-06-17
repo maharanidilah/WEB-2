e2ad79b0b0493e124c5cf9946fc4487f1eeb34af
mhrnidlh <maharaniiidilah@gmail.com>
2025-06-17 19:01:30 +0700

    upload praktikum3

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+
+require_once ("function/callpage.php");
+callpage("navbar");
+callpage("header");
+callpage("about");
+callpage("footer");
+
+?>
---
+++ projects.php
@@ -0,0 +1,52 @@
+<?php
+    include_once ("pages/navbar.php");
+?>
+            <!-- Bagian Projek -->
+            <section class="py-5">
+                <div class="container px-5 mb-5">
+                    <div class="text-center mb-5">
+                        <h1 class="display-5 fw-bolder mb-0"><span class="text-gradient d-inline">Projek Erza Rizfia</span></h1>
+                    </div>
+                    <div class="row gx-5 justify-content-center">
+                        <div class="col-lg-11 col-xl-9 col-xxl-8">
+                            <!-- Kartu Projek 1 -->
+                            <div class="card overflow-hidden shadow rounded-4 border-0 mb-5">
+                                <div class="card-body p-0">
+                                    <div class="d-flex align-items-center">
+                                        <div class="p-5">
+                                            <h2 class="fw-bolder">Projek Aplikasi Estimasi Bensin</h2>
+                                            <p>projek ini bertujuan untuk membantu para pengguna kendaraan mengestimasi kan harga dan jarak tempuh</p>
+                                        </div>
+                                        <img class="img-fluid" src="https://dummyimage.com/300x400/343a40/6c757d" alt="..." />
+                                    </div>
+                                </div>
+                            </div>
+                            <!-- Kartu Projek 2 -->
+                            <div class="card overflow-hidden shadow rounded-4 border-0">
+                                <div class="card-body p-0">
+                                    <div class="d-flex align-items-center">
+                                        <div class="p-5">
+                                            <h2 class="fw-bolder">Projek penyewaan lapangan bola</h2>
+                                            <p>projek ini berpokus untuk seseorang yang ingin memesan atau memboking lapangan bola</p>
+                                        </div>
+                                        <img class="img-fluid" src="https://dummyimage.com/300x400/343a40/6c757d" alt="..." />
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+            <!-- Bagian Ajakan Bertindak -->
+            <section class="py-5 bg-gradient-primary-to-secondary text-white">
+                <div class="container px-5 my-5">
+                    <div class="text-center">
+                        <h2 class="display-4 fw-bolder mb-4">Mari membangun sesuatu bersama</h2>
+                        <a class="btn btn-outline-light btn-lg px-5 py-3 fs-6 fw-bolder" href="contact.html">Hubungi Saya</a>
+                    </div>
+                </div>
+            </section>
+        </main>
+<?php
+    include_once ("pages/footer.php");
+?>
