f79111829a9c6055913313ca70907b09d685c470
mhrnidlh <maharaniiidilah@gmail.com>
2025-06-18 21:30:42 +0700

    upload ulang projek karena github sebelumnya error

---
+++ layout/sidebar.php
@@ -0,0 +1,59 @@
+<!-- Main Sidebar Container -->
+<aside class="main-sidebar sidebar-dark-primary elevation-4">
+    <!-- Brand Logo -->
+    <a href="home.php" class="brand-link">
+        <img src="img/EduDashboard.png" alt="Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
+        <span class="brand-text font-weight-light">Manajemen Dosen</span>
+    </a>
+
+    <!-- Sidebar -->
+    <div class="sidebar">
+        <!-- User Panel -->
+        <div class="user-panel mt-3 pb-3 mb-3 d-flex align-items-center">
+            <div class="image">
+                <img src="dilahcanteq.jpg" class="img-circle elevation-2" alt="User Image">
+            </div>
+            <div class="info">
+                <a href="home.php" class="d-block">dilahcanteq</a>
+            </div>
+        </div>
+
+        <!-- Sidebar Menu -->
+        <nav>
+            <ul class="nav nav-pills nav-sidebar flex-column" role="menu">
+                <li class="nav-header">MENU</li>
+
+                <li class="nav-item">
+                    <a href="dosen.php" class="nav-link">
+                        <i class="nav-icon fas fa-user-tie"></i>
+                        <p>Dosen</p>
+                    </a>
+                </li>
+
+                <li class="nav-item">
+                    <a href="kegiatan.php" class="nav-link">
+                        <i class="nav-icon fas fa-calendar-check"></i>
+                        <p>Kegiatan</p>
+                    </a>
+                </li>
+
+                <li class="nav-item">
+                    <a href="jenis_kegiatan.php" class="nav-link">
+                        <i class="nav-icon fas fa-tags"></i>
+                        <p>Jenis Kegiatan</p>
+                    </a>
+                </li>
+
+                <li class="nav-item">
+                    <a href="penelitian.php" class="nav-link">
+                        <i class="nav-icon fas fa-book-open"></i>
+                        <p>Penelitian</p>
+                    </a>
+                </li>
+
+            </ul>
+        </nav>
+        <!-- /.sidebar-menu -->
+    </div>
+    <!-- /.sidebar -->
+</aside>
